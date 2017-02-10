0c77a5788ba3e3790caf429f90efdbd4a3b1290b
carol <urnotchrissie@outlook.com>
2017-02-10 16:21:49 +0800

    solve POJ 2393 Yogurt factory

---
+++ POJ2393.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+int n;
+int s;
+ll c[10005];
+ll y[10005];
+
+void solve() {
+    ll res = 0;
+    
+    for (int i = 1; i < n; i++) {
+        c[i] = min(c[i], c[i - 1] + s);
+    } 
+    for (int i = 0; i < n; i++) {
+        res += c[i] * y[i];
+    }
+
+    printf("%lld\n", res);
+}
+
+int main() {
+    scanf("%d %d", &n, &s);
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &c[i], &y[i]);
+    }
+    solve();
+    return 0;
+}
